70b6c6d245ccc7ad477fde184113f91048752e4f
Theo Pepler <theopepler@gmail.com>
2021-07-28 13:53:14 +0100

    Rename node argument as index in trajectory function to reflect change in SimInf package

--- R/prep_cts_init.R
+++ R/prep_cts_init.R
@@ -139,7 +139,7 @@ prep_cts_init <- function(modeldata
     #day_u0$S <- SimInf::susceptible(SimInf::run(model))[, 2] # Note: old code; stopped working when 'susceptible' function was replaced by 'trajectory' in SimInf package
     result <- SimInf::trajectory(model = SimInf::run(model),
                                  compartments = 'S',
-                                 node = NULL)
+                                 index = NULL)
     day_u0$S <- result$S[result$time == d + 1]
     #print(dim(tempout)) # debugging
     #print(head(tempout)) # debugging
